4ab62d902648ac6f819a4665a066f98537ffeacb
alex <volohovaleksandr1974@gmail.com>
2024-02-13 23:26:27 +0300

    Все негативные кейсы проверяются сразу, кейс с отсутствием второго аргумента проверяется отдельно

--- source/inverse.js
+++ source/inverse.js
@@ -1,12 +1,9 @@
 'use strict';
 
 function inverse(arr, num) {
-    if (!Array.isArray(arr)) return null;
+    if (!Array.isArray(arr) || (num !== undefined && typeof num !== 'number')) return null;
 
     if (!num) return arr.slice().reverse();
-
-    // пока не особо понимаю, как можно компактнее реализовать проверку на наличие второго аргумента
-    if (typeof num !== 'number') return null;
 
     const reversedPart = (num >= 0) ? arr.slice(num, arr.length).reverse() : arr.slice(0, num).reverse();
     const restPart = (num >= 0) ? arr.slice(0, num) : arr.slice(num);
