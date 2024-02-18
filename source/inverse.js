a3c8995f5d6f1939043c3a9091a9594c5b7084d9
alex <volohovaleksandr1974@gmail.com>
2024-02-18 22:28:54 +0300

    Внес правки в тестах, добавил {}, оптимизировал условия

--- source/inverse.js
+++ source/inverse.js
@@ -10,12 +10,17 @@
  *   ошибочны.
  */
 function inverse(arr, num) {
-    if (!Array.isArray(arr) || (num !== undefined && typeof num !== 'number')) return null;
+    if (!Array.isArray(arr) || (num && typeof num !== 'number')) {
+        return null;
+    }
 
-    if (!num) return arr.slice().reverse();
+    if (!num) {
+        return arr.slice().reverse();
+    }
 
-    const reversedPart = (num >= 0) ? arr.slice(num, arr.length).reverse() : arr.slice(0, num).reverse();
-    const restPart = (num >= 0) ? arr.slice(0, num) : arr.slice(num);
+    const isNumPositive = num >= 0;
+    const reversedPart = isNumPositive ? arr.slice(num, arr.length).reverse() : arr.slice(0, num).reverse();
+    const restPart = isNumPositive ? arr.slice(0, num) : arr.slice(num);
 
-    return (num >= 0) ? restPart.concat(reversedPart) : reversedPart.concat(restPart);
+    return isNumPositive ? restPart.concat(reversedPart) : reversedPart.concat(restPart);
 }
